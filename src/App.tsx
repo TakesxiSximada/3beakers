424e074e63fe0f500831b163232483093fb9a737
TakesxiSximada <8707279+TakesxiSximada@users.noreply.github.com>
2022-02-08 10:24:51 +0900

    ✨ Implement 3 beaker app.

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { connect } from 'react-redux';
 
-function App() {
+import './App.css';
+import { selectBeaker } from './store'
+
+
+const printBeaker = (beaker: any) => `${beaker.currentCount} / ${beaker.maxCount}`;
+
+function App(props: any) {
   return (
     <div className="App">
+      {
+	props.isCompleted() && (
+	    <div>
+	      <span>Game Clear</span>
+	      <span>Congratulations!!</span>
+	    </div>
+	)
+      }
       <header className="App-header">
-        <p>
-          OK
-        </p>
+        {
+          props.beakerList.map(
+            (beaker: any, index: number) => (
+	      <span onClick={props.selectBeaker} data-index={index} key={index}>{printBeaker(beaker)}</span>
+	    )
+	  )
+        }
       </header>
     </div>
   );
 }
 
-export default App;
+const mapStateToProps = (state: any) => {
+  return {
+    name: state.name,
+    beakerList: state.beakerList,
+    isCompleted: () => state.beakerList.some((elm: any) => elm.currentCount == 4),
+  }
+};
+
+const mapDispatchToProps = (dispatch: any) => {
+  return {
+    selectBeaker: (event: any) => {
+      const index = event.currentTarget.getAttribute("data-index");
+      dispatch(selectBeaker(index));
+    }
+  }
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
---
+++ src/store.ts
@@ -0,0 +1,71 @@
+import { createStore } from "redux";
+
+const initialState = {
+  beakerList: [
+    {
+      currentCount: 8,
+      maxCount: 8,
+    },
+    {
+      currentCount: 0,
+      maxCount: 5,
+    },
+    {
+      currentCount: 0,
+      maxCount: 3,
+    },
+  ],
+  sourceIndex: null,
+};
+
+// Action Type
+const SELECT_BEAKER = "SELECT_BEAKER";
+
+// Action Creator
+export const selectBeaker = (index: number) => ({type: SELECT_BEAKER, index: index});
+
+// Reducer
+const reducer = (state = initialState, action: any) => {
+  switch (action.type) {
+    case SELECT_BEAKER:
+      if (state.sourceIndex === null) {
+	return {
+	  ...state,
+	  sourceIndex: action.index,
+	}
+      } else {
+	const srcBeaker = state.beakerList[state.sourceIndex];
+	const destBeaker = state.beakerList[action.index];
+	const avaiableCount = destBeaker.maxCount - destBeaker.currentCount;
+	const movableCount = srcBeaker.currentCount <= avaiableCount ? srcBeaker.currentCount : avaiableCount;
+	const newBeakerList = state.beakerList.map(
+	  (beaker: any, index: number) => {
+	    if (index == state.sourceIndex) {  // source beaker
+	      return {
+		...beaker,
+		currentCount: beaker.currentCount - movableCount,
+	      }
+	    } else if (index == action.index) {  // destination beaker
+	      return {
+		...beaker,
+		currentCount: beaker.currentCount + movableCount,
+	      }
+	    } else {
+	      return beaker;
+	    }
+	  }
+	);
+	return {
+	  ...state,
+	  sourceIndex: null,
+	  beakerList: newBeakerList,
+	}
+      }
+    default:
+      return state;
+  }
+};
+
+const store = createStore(reducer);
+
+export default store;
